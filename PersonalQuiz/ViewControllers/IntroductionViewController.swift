ba330b73192eb093e271043b9f19efcc78e0ba47
Alex Sobolev <31108@mail.ru>
2022-06-14 22:59:29 +0300

    Setup button done on unwind segue

---
+++ PersonalQuiz/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Александр Соболев on 13.06.2022.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    @IBAction func unwind(segue: UIStoryboardSegue) {}
+    
+    }
+
+
