f32b236758e83e98625db8daf1709409b9ec4a90
Alex Sobolev <31108@mail.ru>
2022-06-14 23:35:07 +0300

    Setup outlets and actions on QuestionViewController

--- PersonalQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionsViewController.swift
@@ -8,22 +8,36 @@
 import UIKit
 
 class QuestionsViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var questionProgressView: UIProgressView!
+    
+    @IBOutlet weak var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet weak var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabel: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet weak var rangedStackView: UIStackView!
+    @IBOutlet var rangedLabels: [UILabel]!
+    @IBOutlet weak var rangedSlider: UISlider!
+    
+    
+    
+    private var questions = Question.getQuestions()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
     }
-    */
-
+    @IBAction func multipleAnswerButtonPressed() {
+    }
+    @IBAction func rangedAnswerButtonPressed() {
+    }
 }
