662d0c7d5d593cc390f540a9f308089c79b3572c
Alex Sobolev <31108@mail.ru>
2022-06-14 22:41:48 +0300

    Setup hardcode for questions and answers data

--- PersonalQuiz/Models/Questions.swift
+++ PersonalQuiz/Models/Questions.swift
@@ -7,8 +7,43 @@
 
 struct Question {
     let title: String
-    let answers: [Answer]
     let type: ResponseType
+    let answers: [Answer]
+    
+    static func getQuestions() -> [Question] {
+        [
+            Question(
+                title: "Какую пищу вы предпочитаете?",
+                type: .single,
+                answers: [
+                    Answer(title: "Стейк", animal: .dog),
+                    Answer(title: "Рыба", animal: .cat),
+                    Answer(title: "Морковь", animal: .rabbit),
+                    Answer(title: "Кукуруза", animal: .turtle)
+                ]
+            ),
+            Question(
+                title: "Что вам нравится больше?",
+                type: .multiple,
+                answers: [
+                    Answer(title: "Плавать", animal: .dog),
+                    Answer(title: "Спать", animal: .cat),
+                    Answer(title: "Обниматься", animal: .rabbit),
+                    Answer(title: "Есть", animal: .turtle)
+                ]
+            ),
+            Question(
+                title: "Любите ли вы поездки на автомобиле?",
+                type: .ranged,
+                answers: [
+                    Answer(title: "Ненавижу", animal: .cat),
+                    Answer(title: "Нервничаю", animal: .rabbit),
+                    Answer(title: "Не замечаю", animal: .turtle),
+                    Answer(title: "Обожаю", animal: .dog)
+                ]
+            )
+        ]
+    }
 }
 
 struct Answer {
