2799579f3f9e5a23cec17f9956f69c8d3afe1f93
Alex Sobolev <31108@mail.ru>
2022-06-16 18:58:15 +0300

    Setup text in project and shrifts

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -14,9 +14,22 @@ class ResultViewController: UIViewController {
     
     var answers: [Answer]!
     
+    //MARK: - Background color change
+      private let firstColor = UIColor(
+          red: 120/255,
+          green: 200/255,
+          blue: 180/255,
+          alpha: 1)
+      private let secondColor = UIColor(
+          red: 160/255,
+          green: 150/255,
+          blue: 200/255,
+          alpha: 1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
+        view.addVerticalGradientLayer(topColor: firstColor, bottomColor: secondColor)
         updateResult()
     }
     
